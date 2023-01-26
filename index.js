ab5b462160a6381ee48f76be4011bf4189156ea1
Ritik chourasiya <ritvikchoure65+git@gmail.com>
2023-01-26 22:51:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const redis = require("redis");
 const cors = require("cors");
-let RedisStore = require("connect-redis")(session);
 
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
-  REDIS_URL,
-  SESSION_SECRET,
-  REDIS_PORT,
 } = require("./config/config");
-
-let redisClient = redis.createClient({
-  host: REDIS_URL,
-  port: REDIS_PORT,
-});
 
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -45,19 +34,6 @@ connectWithRetry();
 
 app.enable("trust proxy");
 app.use(cors({}));
-app.use(
-  session({
-    store: new RedisStore({ client: redisClient }),
-    secret: SESSION_SECRET,
-    cookie: {
-      secure: false,
-      resave: false,
-      saveUninitialized: false,
-      httpOnly: true,
-      maxAge: 30000,
-    },
-  })
-);
 
 app.use(express.json());
 
